2aa452af81a4fe3853debc8fdbe2a512ce7e17a4
refren666 <denys86shvaiber@gmail.com>
2022-01-25 16:12:59 +0200

    42

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import {useContext} from "react";
 import {AuthContext} from "../hoc/AuthProvider";
 
-// useContext - відловлює/приймає значення контексту
+// useContext - відловлює/приймає значення контексту: value = { user, login, logout }
 export const useAuth = () => useContext(AuthContext)
