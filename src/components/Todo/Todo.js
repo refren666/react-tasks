d8d29bbca5dd781e1f835a08f0911093d4945511
refren666 <denys86shvaiber@gmail.com>
2022-02-02 12:49:52 +0200

    redux practise w/ todos

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,17 +2,11 @@ import React from 'react';
 import {useDispatch, useSelector} from "react-redux";
 
 import todoListReducer, {changeStatus, deleteTodo} from "../../store/todoList.slice";
-import css from "./Todo.module.css";
+import classes from "./Todo.module.css";
 
 const Todo = ({todo}) => {
   const {todos} = useSelector(state => state['todoListReducer'])
   const dispatch = useDispatch();
-  const completed = todos[0].completed;
-  const classes = [];
-
-  if (completed) {
-    classes.push('done')
-  }
 
   const deleteTodoItem = (id) => {
     dispatch(deleteTodo({id}))
@@ -24,9 +18,11 @@ const Todo = ({todo}) => {
   }
 
   return (
-    <div style={{display: 'flex'}}>
-      <input onClick={() => changeTodoStatus(todo.id)} type="checkbox"/>
-      <span className={classes.join(' ')}>
+    <div className={classes.wrapper}>
+      <input onChange={() => changeTodoStatus(todo.id)} type="checkbox"/>
+      <span className={
+        todo.completed ? classes.done : classes.unfinished
+      }>
         {todo.todo}
       </span>
       <button onClick={() => deleteTodoItem(todo.id)}>Delete</button>
